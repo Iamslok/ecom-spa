8e93faf04e2813c247f00cff24efa8677ff8233a
sailokesh <sailokeshiamslok@gmail.com>
2024-08-12 18:04:30 +0530

    modified auto complete search functionality

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartserviceService } from '../cartservice.service';
-import { CartItemsModel } from '../../assets/Models/Products';
+import { CartItemsModel, ProductModel } from '../../assets/Models/Products';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,24 +12,26 @@ export class NavBarComponent implements OnInit {
 
   itemCount: number = 0;
   searchTerm: string = '';
-  filteredProducts: CartItemsModel[] = []
-  products: CartItemsModel[] = []
+  filteredProducts: ProductModel[] = []
+  products: ProductModel[] = []
 
-  constructor(private CartService: CartserviceService) { }
+  constructor(private CartService: CartserviceService, private apiService:ApiService) { }
 
   ngOnInit() {
     this.CartService.cartItems$.subscribe(items => {
       this.itemCount = items.length;
     });
 
-    this.products = this.CartService.getCartItemsFromLocal()
+    this.apiService.getAllProducts().subscribe(res=>{
+      this.products = res;
+    })
   }
 
   onSearch() {
     const term = this.searchTerm.toLowerCase();
     if(term != "" && term != undefined && term != null){
       this.filteredProducts = this.products.filter(product => 
-        product && product.PrductName && product.PrductName.toLowerCase().includes(term)
+        product && product.name && product.name.toLowerCase().includes(term)
       );
     }else{
       this.filteredProducts = []
